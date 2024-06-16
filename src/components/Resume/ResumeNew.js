b8f8e940685d52d7bc17aa2f0dfed92b595725d1
SUDANA SHASHI KIRAN <141379562+shashi36-github@users.noreply.github.com>
2024-06-16 17:18:29 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/AlgoUniversity_Student_Resume_Template";
+import pdf from "../../Assets/../Assets/AlgoUniversity_Student_Resume_Template.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
